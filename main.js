6eec16a90ddfab7c557bd93b02d96ea56c156f28
Anna <annamura833@gmail.com>
2020-03-26 11:55:31 +0100

    Esercizio completo

---
+++ main.js
@@ -0,0 +1,29 @@
+// Fizz Buzz test:
+//  Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero
+// e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
+
+
+
+var fizz = "Fizz";
+var buzz = "Buzz";
+var fizzbuzz = "FizzBuzz";
+
+
+for (var i = 1; i <= 100; i++) {  // Numeri da 1 a 100
+
+  if ((i % 3 === 0) && (i % 5 === 0)) {   //Stampa i multipli sia di 3 che di 5
+    console.log(fizzbuzz);
+  }
+  else if (i % 3 === 0) {                 //Stampa i multipli solo di 3
+   console.log(fizz);
+  }
+   else if (i % 5 === 0) {                //Stampa i multipli solo di 5
+   console.log(buzz);
+   }
+
+   else {                                 //Stampa tutti gli altri
+     console.log(i);
+   }
+}
